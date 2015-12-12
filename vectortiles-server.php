7b72a3940e0b0e8d42cfb16e7d127ac08a2f46c2
Jianxuan Li <la5c@qq.com>
2015-12-12 22:13:06 +0800

    add tile server and php-redis handle

---
+++ vectortiles-server.php
@@ -0,0 +1,8 @@
+<?php
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+echo $redis->get($_GET['zoom'] ."-". $_GET['x'] . "-" .$_GET['y']);
+$redis->close();
+
